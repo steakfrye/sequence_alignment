ce1bfaafecccfdf30f654a4d792935260acbc689
Dylan Frye <dylanfrye@Dylans-iMac.local>
2018-10-11 12:40:23 -0500

    Cleaned up syntax.

--- sequence_alignment.py
+++ sequence_alignment.py
@@ -6,47 +6,47 @@ sequence2 = TATGC
 def NWScore(sequence1, sequence2):
     Score(0,0) = 0
 
-   for j=1 to length(sequence2):
-     Score(0,j) = Score(0,j-1) + Ins(sequence2[j])
+    for j = 1 to length(sequence2):
+        Score(0, j) = Score(0, j - 1) + Ins(sequence2[j])
 
-   for i=1 to length(sequence1):
-     Score(i,0) = Score(i-1,0) + Del(sequence1[i])
+    for i = 1 to length(sequence1):
+        Score(i, 0) = Score(i - 1,0) + Del(sequence1[i])
 
-     for j=1 to length(sequence2):
-       scoreSub = Score(i-1,j-1) + Sub(sequence1[i], sequence2[j])
-       scoreDel = Score(i-1,j) + Del(sequence1[i])
-       scoreIns = Score(i,j-1) + Ins(sequence2[j])
-       Score(i,j) = max(scoreSub, scoreDel, scoreIns)
+        for j = 1 to length(sequence2):
+            sub_score = Score(i - 1,j - 1) + (sequence1[i] - sequence2[j])
+            del_score = Score(i - 1, j) + Del(sequence1[i])
+            ins_score = Score( i, j - 1) + Ins(sequence2[j])
+            Score(i, j) = max(sub_score, del_score, ins_score)
 
-   for j=0 to length(sequence2):
-     LastLine(j) = Score(length(sequence1),j)
-   return LastLine
+        for j = 0 to length(sequence2):
+         LastLine(j) = Score(length(sequence1),j)
+        return LastLine
 
 def Hirschberg(sequence1, sequence2):
-   Z = ""
-   W = ""
-   if length(sequence1) == 0:
-       for i=1 to length(sequence2):
-           Z = Z + '-'
-           W = W + sequence2i
+    Z = ""
+    W = ""
+    if length(sequence1) == 0:
+        for i = 1 to length(sequence2):
+            Z = Z + '-'
+            W = W + sequence2i
 
     else if length(sequence2) == 0:
-        for i=1 to length(sequence1):
+        for i = 1 to length(sequence1):
             Z = Z + sequence1[i]
             W = W + '-'
 
     else if length(sequence1) == 1 or length(sequence2) == 1:
-        (Z,W) = NeedlemanWunsch(sequence1,sequence2)
+        (Z, W) = NeedlemanWunsch(sequence1, sequence2)
 
     else:
-        sequence1len = length(sequence1)
-        sequence1mid = length(sequence1)/2
-        sequence2len = length(sequence2)
+        sequence1_len = length(sequence1)
+        sequence1_mid = length(sequence1)/2
+        sequence2_len = length(sequence2)
 
-    ScoreL = NWScore(sequence11:sequence1mid, sequence2)
+    ScoreL = NWScore(sequence11:sequence1_mid, sequence2)
     ScoreR = NWScore(rev(Xxmid+1:sequencelen), rev(sequence2))
-    sequence2mid = arg max ScoreL + rev(ScoreR)
+    sequence2_mid = arg max ScoreL + rev(ScoreR)
 
-    (Z,W) = Hirschberg(sequence1:xmid, sequence21:sequence2mid) + Hirschberg(sequencexmid+1:xlen, sequence2sequence2mid+1:sequence2len)
+    (Z, W) = Hirschberg(sequence1:xmid, sequence21:sequence2mid) + Hirschberg(sequencexmid+1:xlen, sequence2sequence2mid+1:sequence2len)
 
-    return (Z,W)
+    return (Z, W)
